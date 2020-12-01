2a321633183de4a187654daf1c638978d7068990
Douglasyeh <69801580+Douglasyeh@users.noreply.github.com>
2020-12-01 14:44:44 +0800

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,58 @@
+var data = [
+    {
+        idx: 1,
+        name: "小豬",
+        money: 12000,
+        age: 12,
+        gender: "boy"
+    },
+    {
+        idx: 2,
+        name: "小烏龜",
+        money: 52000,
+        age: 20,
+        gender: "girl"
+    },
+    {
+        idx: 3,
+        name: "小鳥",
+        money: 150,
+        age: 16,
+        gender: "boy"
+    },
+    {
+        idx: 4,
+        name: "小貓",
+        money: 360000,
+        age: 35,
+        gender: "boy"
+    },
+    {
+        idx: 5,
+        name: "小狗",
+        money: 650,
+        age: 19,
+        gender: "boy"
+    },
+    {
+        idx: 6,
+        name: "鱷魚",
+        money: 9999,
+        age: 40,
+        gender: "girl"
+    },
+    {
+        idx: 7,
+        name: "龍蝦",
+        money: 10,
+        age: 2,
+        gender: "girl"
+    },
+    {
+        idx: 8,
+        name: "海藻",
+        money: 500,
+        age: 30,
+        gender: "boy"
+    }
+]
